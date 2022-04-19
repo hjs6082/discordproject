3378f932a2dd6f3efddf1c4327c97cb4c730e5e3
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2022-04-19 19:13:22 +0900

    앨범 시스템 오류 수정후 업데이트 완료

--- Assets/02.Scripts/Album/AlbumImage.cs
+++ Assets/02.Scripts/Album/AlbumImage.cs
@@ -8,7 +8,7 @@ using DG.Tweening;
 public class AlbumImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     [SerializeField]
-    private Outline outline;
+    private UnityEngine.UI.Outline outline;
     [SerializeField]
     private GameObject fullPictureObj;
     [SerializeField]
@@ -16,17 +16,25 @@ public class AlbumImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        outline = this.gameObject.GetComponent<UnityEngine.UI.Outline>();
+        outline.effectColor = new Color(outline.effectColor.r, outline.effectColor.g, outline.effectColor.b, 128);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
+        outline.effectColor = new Color(outline.effectColor.r, outline.effectColor.g, outline.effectColor.b, 0);
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        
-        StartCoroutine(Close());
+        Close();
     }
 
-    IEnumerator Close()
+    public void Close()
+    {
+        fullPictureObj.SetActive(false);
+        album.SetActive(true);
+    }
+
+/*    IEnumerator Close() 이곳에 닷트윈을 쓰지 않음으로 인하여 폐기
     {
         Cursor.lockState = CursorLockMode.Locked;
         fullPictureObj.GetComponent<RectTransform>().DOLocalRotate(new Vector3(0, 0, -20f), 1.5f);
@@ -34,5 +42,5 @@ public class AlbumImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         album.SetActive(true);
         fullPictureObj.SetActive(false);
         Cursor.lockState = CursorLockMode.None;
-    }
+    }*/
 }
--- Assets/02.Scripts/Album/Picture.cs
+++ Assets/02.Scripts/Album/Picture.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.EventSystems;
 
-public class Picture : MonoBehaviour
+public class Picture : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private GameObject album;
-
     FullPicture fullPicture;
     private Sprite sendSprite;
     [SerializeField]
     private GameObject fullPictureObj;
     [SerializeField]
     private Image fullPictureImage;
+
+    [SerializeField]
+    private UnityEngine.UI.Outline outline;
 
     private void Start()
     {
@@ -30,22 +33,33 @@ public class Picture : MonoBehaviour
     
     public void PictureSend()
     {
-        StartCoroutine(RotateAndPlay(sendSprite));
+        PlayPicture(sendSprite);
     }
 
-/*    public void RotateAndPlay(Sprite _sprite)
+    public void PlayPicture(Sprite _sprite)
     {
-
-    }*/
-
-    IEnumerator RotateAndPlay(Sprite _sprite)
-    {
-        Cursor.lockState = CursorLockMode.Locked;
         fullPictureImage.GetComponent<Image>().sprite = _sprite;
-        fullPictureObj.GetComponent<RectTransform>().DOLocalRotate(new Vector3(0, 0, 0), 1.5f);
-        yield return new WaitForSeconds(1.5f);
-        Debug.Log("423");
-        Cursor.lockState = CursorLockMode.None;
-        album.SetActive(false);
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        outline = this.gameObject.GetComponent<UnityEngine.UI.Outline>();
+        outline.effectColor = new Color(outline.effectColor.r, outline.effectColor.g, outline.effectColor.b, 128);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        outline.effectColor = new Color(outline.effectColor.r, outline.effectColor.g, outline.effectColor.b, 0);
+    }
+
+    /*    IEnumerator RotateAndPlay(Sprite _sprite) // 닷트윈 삭제로 인하여 쓰지않습니다.
+        {   
+            Cursor.lockState = CursorLockMode.Locked;
+            fullPictureImage.GetComponent<Image>().sprite = _sprite;
+            fullPictureObj.GetComponent<RectTransform>().DOLocalRotate(new Vector3(0, 0, 0), 1.5f);
+            yield return new WaitForSeconds(1.5f);
+            Debug.Log("423");
+            Cursor.lockState = CursorLockMode.None;
+            album.SetActive(false);
+        }*/
 }
