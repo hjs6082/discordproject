22e5245f711048a94cf719d3b2df73bb9a37c4c0
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2022-05-18 18:51:11 +0900

    버그 수정

--- Assets/02.Scripts/PasswordPaperUI.cs
+++ Assets/02.Scripts/PasswordPaperUI.cs
@@ -3,35 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PasswordPaperUI : MonoBehaviour//, IPointerClickHandler
+public class PasswordPaperUI : MonoBehaviour, IPointerClickHandler
 {
-    // public Item photoData;
-    public GameObject clearPanel;
+    public GameObject passwordPanel;
     public GameObject player;
-    //public GameObject photoObj;
-    /*    public void OnPointerClick(PointerEventData eventData)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            this.gameObject.SetActive(false);
-            Inventory.instance.AddItem(photoData);
-            player.GetComponent<Suntail.PlayerController>().enabled = true;
-            Inventory.instance.FreshSlot();
-            photoObj.SetActive(false);
-        }*/
 
-    public void OnEnable()
-    {
-        StartCoroutine(Chapter1Clear());
-    }
 
-    IEnumerator Chapter1Clear()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        yield return new WaitForSeconds(5f);
-        clearPanel.SetActive(true); //클리어패널이아니라 씬이동으로 바꿀것
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        player.GetComponent<Suntail.PlayerController>().enabled = true;
         this.gameObject.SetActive(false);
+        player.GetComponent<Suntail.PlayerController>().enabled = true;
     }
+
+
 }
--- Assets/02.Scripts/UIPhotoScript.cs
+++ Assets/02.Scripts/UIPhotoScript.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UIPhotoScript : MonoBehaviour, IPointerClickHandler
+public class UIPhotoScript : MonoBehaviour//, IPointerClickHandler
 {
     public GameObject player;
-    public void OnPointerClick(PointerEventData eventData)
+    public GameObject clearPanel;
+    /*    public void OnPointerClick(PointerEventData eventData)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            this.gameObject.SetActive(false);
+            player.GetComponent<Suntail.PlayerController>().enabled = true;
+        }*/
+
+    public void OnEnable()
     {
+        StartCoroutine(Chapter1Clear());
+    }
+
+    IEnumerator Chapter1Clear()
+    {
+        yield return new WaitForSeconds(5f);
+        clearPanel.SetActive(true); //클리어패널이아니라 씬이동으로 바꿀것
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        this.gameObject.SetActive(false);
         player.GetComponent<Suntail.PlayerController>().enabled = true;
+        this.gameObject.SetActive(false);
     }
 }
