71b0102234485400858927b29e4bd1b1d62c20f0
sunwoong-m <datanet231@gmail.com>
2022-05-25 23:50:16 +0900

    오류 조금 수정

--- Assets/SeonWoong/3D/Scripts/CheckLists.cs
+++ Assets/SeonWoong/3D/Scripts/CheckLists.cs
@@ -9,10 +9,14 @@ public class CheckLists : MonoBehaviour
     
     public static void AddCheckList(string _str)
     {
+        GameManager.instance.book.SetActive(true);
+
         GameObject check = Instantiate(GameManager.Instance.check_Prefab, GameManager.Instance.book.GetComponent<Book_Main>().checkList_Parent);
         check.GetComponent<TextMeshProUGUI>().text = _str;
         check.GetComponentInChildren<Toggle>().isOn = false;
 
         GameManager.Instance.book.GetComponent<Book_Main>().checkList_List.Add(check);
+
+        GameManager.instance.book.SetActive(false);
     }
 }
